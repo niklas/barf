a05073c2f795635e5547b1d8efac36b48088f94b
Niklas Hofer <niklas+dev@lanpartei.de>
2009-06-14 00:12:49 +0200

    message listing

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,10 @@
 class Message < ActiveRecord::Base
+  validates_length_of :body, :in => 5..200
+  def due_at
+    Time.now + lifespan
+  end
+
+  def lifespan
+    read_attribute('lifespan') || 5.days
+  end
 end
