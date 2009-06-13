2fe0ea8681f903f949f5d43b4e0eb14cb80f47f9
Niklas Hofer <niklas+dev@lanpartei.de>
2009-06-14 00:22:58 +0200

    nicer creation form

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,18 @@
 class Message < ActiveRecord::Base
+  Lifespans = [
+    ['5 Minuten',  5.minutes],
+    ['15 Minuten', 15.minutes],
+    ['1 Stunde',   1.hour],
+    ['3 Stunden',  3.hours],
+    ['12 Stunden', 12.hours],
+    ['23 Stunden', 23.hours],
+    ['1 Tag',       1.day],
+    ['5 Tage',      5.days],
+    ['1 Woche',     1.week],
+    ['1 Monat',     1.month],
+    ['3 Monate',    3.months],
+    ['ein halbes Jahr', 6.months]
+  ]
   validates_length_of :body, :in => 5..200
   def due_at
     Time.now + lifespan
