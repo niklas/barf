124fc299f619276000935be6d1a9b95a6b5db9d6
Niklas Hofer <niklas+dev@lanpartei.de>
2009-06-17 00:28:30 +0200

    tabs, first try

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,17 @@ module ApplicationHelper
     return if things.blank?
     opts[:class] ||= ''
     opts[:class] += " list #{things.first.class.table_name}"
-    content_tag(:div, render(:partial => things), opts)
+    returning '' do |html|
+      if title = opts.delete(:title)
+        html << tab(title)
+      end
+      html << content_tag(:div, render(:partial => things), opts)
+    end
+  end
+
+  def tab(title, opts={})
+    name = title.underscore.downcase
+    content_for(:tabs) { content_tag(:a, title, opts.merge(:href => "##{name}", :class => 'tab')) }
+    content_tag(:h1, title, opts.merge(:id => name, :class => 'tab'))
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $('a.tab').click( function() {
+    $('h1.tab + div').hide();
+    $( this.hash ).show();
+  });
+  $('a.tab:first + div').click();
+});
