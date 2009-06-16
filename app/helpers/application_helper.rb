c44aab08e41edf91bbaa690f337f596ec232db90
Niklas Hofer <niklas+dev@lanpartei.de>
2009-06-16 22:07:13 +0200

    list_of helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def list_of(things, opts={})
+    return if things.blank?
+    opts[:class] ||= ''
+    opts[:class] += " list #{things.first.class.table_name}"
+    content_tag(:div, render(:partial => things), opts)
+  end
 end
