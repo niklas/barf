f05408de680c890e757a4d10e3a23b6f58df27d8
Niklas Hofer <niklas+dev@lanpartei.de>
2009-06-17 16:18:59 +0200

    create symlinks on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,3 +11,14 @@ single_target = ENV['TARGET'] || "schnurr.local"
 role :app, single_target
 role :web, single_target
 role :db,  single_target, :primary => true
+
+
+namespace :deploy do
+  desc "More symlinks"
+  task :after_symlink, :roles => :app do
+    config_dir = "#{deploy_to}/shared/config"
+    run "mkdir -p #{config_dir}"
+    run "ln -fs #{config_dir}/database.yml #{current_release}/config/database.yml"
+    run "ln -fs #{config_dir}/app_config.yml #{current_release}/config/app_config.yml"
+  end
+end
